2a27b4fda36a8ff9e945dfa646833a3776b1e384
Jessica Armstrong <jeyajay1997@gmail.com>
2025-04-10 14:36:32 +0000

    added pages router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
 
 function App() {
   
 
   return (
    <div>
-    Home 
+  
+
+    <BrowserRouter>
+    <Routes>
+      <Route index element ={<Home />} />
+      <Route path ='/about' element ={<About />} />
+    
+    </Routes>
+    </BrowserRouter>
    </div>
   )
 }
